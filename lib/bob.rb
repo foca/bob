0be3bccfba61a74f9e178d9f0c6d793bb570521a
Simon Rozet <simon@rozet.name>
2009-04-12 23:48:29 +0200

    Move buildable API description into README

--- lib/bob.rb
+++ lib/bob.rb
@@ -10,40 +10,8 @@ require "bob/scm/git"
 require "bob/background_engines"
 
 module Bob
-  # Builds the specified <tt>buildable</tt>. This object must understand the following API:
-  #
-  # * <tt>buildable.kind</tt>
-  #
-  #   Should return a Symbol with whatever kind of repository the buildable's code is
-  #   in (:git, :svn, etc).
-  # * <tt>buildable.uri</tt>
-  #
-  #   Returns a string like "git://github.com/integrity/bob.git", pointing to the code
-  #   repository.
-  # * <tt>buildable.branch</tt>
-  #
-  #   What branch of the repository should we build?
-  # * <tt>buildable.build_script</tt>
-  #
-  #   Returns a string containing the build_script to be run when "building".
-  # * <tt>buildable.start_building(commit_id)</tt>
-  #
-  #   `commit_id` is a String that contains whatever is appropriate for the repo type,
-  #   so it would be a SHA1 hash for git repos, or a numeric id for svn, etc. This is a
-  #   callback so the buildable can determine how long it takes to build. It doesn't
-  #   need to return anything.
-  # * <tt>buildable.add_build(commit_id, build_status, build_output)</tt>
-  #
-  #   Callback for when the build finishes. It doesn't need to return anything. It will
-  #   receive a string with the commit identifier, a boolean for the build exit status
-  #   (true for successful builds, false fore failed ones) and a string with the build
-  #   output (both STDOUT and STDERR).
-  #
-  #   A successful build is one where the build script returns a zero status code.
-  #
-  # The build process is to fetch the code from the repository (determined by
-  # <tt>buildable.kind</tt> and <tt>buildable.uri</tt>), then checkout the specified
-  # <tt>commid_ids</tt>, and finally run <tt>buildable.build_script</tt> on each.
+  # Builds the specified <tt>buildable</tt>. This object must understand
+  # the API described in the README.
   def self.build(buildable, *commit_ids)
     commit_ids.each do |commit_id|
       Builder.new(buildable, commit_id).build
