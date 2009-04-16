76a81eb673faeb4819797154d61ff806c38c4c7c
Simon Rozet <simon@rozet.name>
2009-04-16 16:39:16 +0200

    Move BuildableStub under TestHelper namespace

--- test/helper/buildable_stub.rb
+++ test/helper/buildable_stub.rb
@@ -1,33 +1,35 @@
-class BuildableStub
-  attr_reader :builds, :metadata
+module TestHelper
+  class BuildableStub
+    attr_reader :builds, :metadata
 
-  def initialize(repo)
-    @repo     = repo
-    @builds   = {}
-    @metadata = {}
-  end
+    def initialize(repo)
+      @repo     = repo
+      @builds   = {}
+      @metadata = {}
+    end
 
-  def kind
-    :git
-  end
+    def kind
+      :git
+    end
 
-  def uri
-    @repo.path
-  end
+    def uri
+      @repo.path
+    end
 
-  def branch
-    "master"
-  end
+    def branch
+      "master"
+    end
 
-  def build_script
-    "./test"
-  end
+    def build_script
+      "./test"
+    end
 
-  def start_building(commit_id, commit_info)
-    @metadata[commit_id] = commit_info
-  end
+    def start_building(commit_id, commit_info)
+      @metadata[commit_id] = commit_info
+    end
 
-  def finish_building(commit_id, status, output)
-    @builds[commit_id] = [status ? :successful : :failed, output]
+    def finish_building(commit_id, status, output)
+      @builds[commit_id] = [status ? :successful : :failed, output]
+    end
   end
 end
