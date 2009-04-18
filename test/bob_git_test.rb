df0c4fbfae97b8de288789bd5db444dfd8c311fb
Simon Rozet <simon@rozet.name>
2009-04-18 13:46:42 +0200

    Get rid BobGitTest#commit_id

--- test/bob_git_test.rb
+++ test/bob_git_test.rb
@@ -1,20 +1,22 @@
 require File.dirname(__FILE__) + "/helper"
 
 class BobGitTest < Test::Unit::TestCase
-  attr_accessor :repo, :commit_id, :buildable
+  attr_reader :repo, :buildable
 
   def setup
     super
 
     @repo      = GitRepo.new(:test_repo)
     @repo.create
-    @repo.add_successful_commit
 
-    @commit_id = repo.commits.first[:identifier]
     @buildable = GitBuildableStub.new(@repo)
   end
 
   test "with a successful build" do
+    repo.add_successful_commit
+
+    commit_id = repo.commits.first[:identifier]
+
     Bob.build(buildable, commit_id)
 
     status, output = buildable.builds[commit_id]
@@ -30,6 +32,7 @@ class BobGitTest < Test::Unit::TestCase
 
   test "with a failed build" do
     repo.add_failing_commit
+
     commit_id = repo.commits.first[:identifier]
 
     Bob.build(buildable, commit_id)
@@ -48,9 +51,11 @@ class BobGitTest < Test::Unit::TestCase
   test "with multiple commits" do
     2.times { repo.add_failing_commit }
     commits = repo.commits.collect { |c| c[:identifier] }
+
     Bob.build(buildable, commits)
 
-    assert_equal 3, buildable.metadata.length
-    assert_equal 3, buildable.builds.length
+    assert_equal 2, commits.length
+    assert_equal 2, buildable.metadata.length
+    assert_equal 2, buildable.builds.length
   end
 end
